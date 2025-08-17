b38fb6b79f7ff4dbbbfba94c504c8dda6e2f040d
1034989 <1034989@student.sae.edu.au>
2025-08-17 20:07:21 +1000

    Jungle Drums script push
    Created and pseudo coded the scripts for Jungle Drums

---
+++ Assets/JungleDrums/JungleDrumsScripts/Ballsystem.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballsystem : MonoBehaviour
+{
+    // Deals with the interactions of the ball, ie its destruction, collision etc.
+}
